ccd3469031b91966684e3d4b3915817a9907cb92
LuisESu <lesuarez210@gmail.com>
2022-07-28 19:21:57 -0500

    Se termino

---
+++ Prueba_2/TestDev/Models/usuariosBD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDev.Models
+{
+    class usuariosBD
+    {
+        private string connectionString = "Data Source=KIKE-GARCIA; Initial Catalog=Pruebas; User=power;Password=asdf1234";
+    }
+
+    using(SqlConnection connection = new SqlConnection ){
+
+        }
+
